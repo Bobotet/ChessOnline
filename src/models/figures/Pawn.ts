cd22d56db73fdeb7eadcbf467ef51fd790cff0aa
Ilya <gurinovich-ilya@list.ru>
2023-10-05 16:27:39 +0300

    fix first pawn move

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -21,9 +21,18 @@ export default class Pawn extends Figure {
     const direction = this.color === 'BLACK' ? 1 : -1;
     const firstStepDirection = this.color === 'BLACK' ? 2 : -2;
     if (
-      (target.y === this.cell.y + direction || (this.isFirstStep && target.y === this.cell.y + firstStepDirection)) &&
+      target.y === this.cell.y + direction &&
       target.x === this.cell.x &&
       this.cell.board.getCell(target.x, target.y).isEmpty()
+    ) {
+      return true;
+    }
+    if (
+      this.isFirstStep &&
+      target.y === this.cell.y + firstStepDirection &&
+      this.cell.board.getCell(target.x, target.y).isEmpty() &&
+      this.cell.board.getCell(target.x, target.y - direction).isEmpty() &&
+      target.x === this.cell.x
     ) {
       return true;
     }
